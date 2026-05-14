1bb6d0619de5391e24b0a01ce27eb2d9fdd21703
Vitorio Guilherme <vitorioguilherme035@gmail.com>
2026-05-14 11:06:55 -0300

    ex-básicos

---
+++ logica-com-typescript/contador.ts
@@ -0,0 +1,32 @@
+let contador = 0
+console.log("--------------while-----------")
+while(contador<=10){
+	console.log(contador)
+	contador++
+}
+
+ contador = 10
+
+ console.log("-------------do while----------")
+
+ do {
+
+	 console.log(contador)
+	 contador--
+}while(contador>=0)
+
+console.log("-------------for----------------")
+
+for(let i=0; i<=10; i++){
+	console.log(i)
+}
+
+console.log("-------------for --------------")
+
+for (let i=10; i>=0; i--){
+	console.log(i)
+}
+
+ 
+ 
+
